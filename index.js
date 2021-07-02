2aa4746d7bc1bac1f613356f2f3b37584af33bd5
Madhav <sbmadhav@gmail.com>
2021-07-02 02:38:10 -0400

    chat server update 1

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-
-const app = new Application();
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
 app.use((ctx) => {
   ctx.response.body = "Hello World Third try!";
 });
 
+
+const messages = [];
+
+const router = new Router();
+router
+  .get("/", ({response}) => {
+    response.body = "Chat Server";
+  })
+  .get("/messages", ({response}) => {
+    response.body = messsages;
+  })
+  .post("/message", async({request}) => {
+    const message = await request.body().value;
+    messages.push(message);
+  });
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
 addEventListener("fetch", app.fetchEventHandler());
