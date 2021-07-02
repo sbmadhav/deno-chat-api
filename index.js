c8bd532caf44804a840260082b8d18ffc3ea04db
Madhav <sbmadhav@gmail.com>
2021-07-02 02:14:36 -0400

    Fixing the fetch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 addEventListener("fetch", (evt) => {
-    const response = new Response("Hello World!"), {
-        headers: { "content-type": "text/plain" },
+    const response = new Response("Hello World!", {
+      headers: { "content-type": "text/plain" },
     });
     evt.respondWith(response);
 });
